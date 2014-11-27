24c76904e187064194f14d50ee0b0c7824614f10
Jacob Rasmussen <jacrras@gmail.com>
2014-11-27 17:35:51 +0100

    added sample level to app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,102 @@
 gamvas.config.preventKeyEvents = false;
 game = new mainState('game');
 
+var samplelevel = '{\n\
+"H": "player",\n\
+"M": "obstacle",\n\
+"!": "danger",\n\
+"E": "enemy",\n\
+"o": "collectible",\n\
+"Y": "goal"\n\
+}\n\
+\n\
+BEGINLEVEL\n\
+\n\
+\n\
+H                                    oooo\n\
+MMMMMMMMMM!!!!!!!!!MMMMMMMMM         MMMM\n\
+\n\
+\n\
+\n\
+    o                  E\n\
+    o   MMMMMMMMM!!!MMMMMMMMMMMMMMMMM!!!!\n\
+    o\n\
+    o          oo    ooo\n\
+   MMMM        MM    MMM\n\
+\n\
+                 E\n\
+!!!!!!!MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM  M\n\
+\n\
+   Y       MMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n\
+MMMMMMMMMMM\n\
+\n\
+\n\
+\n\
+BEGINLEVEL\n\
+\n\
+     oo          E      M\n\
+     oo       watchit!  M\n\
+     oo                 M             !\n\
+     oo                 !             !\n\
+     oo                               !\n\
+     UP                               M\n\
+                                      Moo\n\
+                        M             Moo\n\
+                        M             CoW\n\
+                   Way  M             MMMMMM\n\
+                        M\n\
+                        M             MMMMM\n\
+                  ^     M                 oM      MMMM\n\
+                        M             MMMMM\n\
+            this        M                 !M\n\
+                        M             MMMMM\n\
+                        M                 oM             M\n\
+o      H                M   Pillow    MMMMM         E    M\n\
+MMMMMMMMMMMMM!!!!MMMMMMMMMMM!!!!!!MMMM Y      MMMMMMMMMMMM\n\
+                                      MMMMMMMM\n\
+\n\
+    E     E      E       E       E      E      E\n\
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n\
+\n\
+\n\
+\n\
+BEGINLEVEL\n\
+\n\
+\n\
+\n\
+H\n\
+MMMMM             MMMMMMMMMMMMMMMMMM  MMMMMMMM             MMMMMMMMMMMM\n\
+                                                                      M\n\
+                                     M                                M\n\
+                    ooo        E                                      MMMM\n\
+                 M  MMM    MMMMMMMMM                                     M\n\
+                                                                         M\n\
+                                                                     M\n\
+                                                        !  o  !\n\
+                                                       ! ! M ! !  M   !\n\
+                                                      !   ! !   !     !\n\
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!     !     !!!!!!!!MMMMMMM\n\
+\n\
+\n\
+\n\
+\n\
+\n\
+\n\
+\n\
+                                                                                                                                        !thanks for plaYing!\n\
+o                                                                                                E                      E\n\
+M            M               M               M     adrenaline      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n\
+\n\
+\n\
+\n\
+\n\
+\n\
+\n\
+\n\
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
+\n\
+\n';
+
 gamvas.event.addOnLoad(function() {
     var testlevel = '{\n\
     "H": "player",\n\
@@ -55,7 +151,7 @@ function runThis() {
 }
 
 function trySample() {
-    document.getElementById('textInput').value = testlevel2;
+    document.getElementById('textInput').value = samplelevel;
     runThis();
 }
 
