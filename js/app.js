5ba453eae543aaa50c4cd2d19fb1f0a71a5db0cc
Per Mortensen <per@permortensen.com>
2014-11-27 16:07:05 +0100

    File loading now puts value into textbox

--- js/app.js
+++ js/app.js
@@ -53,15 +53,7 @@ BEGINLEVEL\n\
 ffffff  hi! jkljkffffffffffffffffffffljkl\n\
 \n';
 
-    var parser = new Parser();
-    try {
-        parser.parse(testlevel);
-    } catch(e) {
-        alert(e);
-        return;
-    }
-
-    startGame(parser.levels);
+    parseAndStartGame(testlevel);
 });
 
 function fileSelected(event) {
@@ -69,21 +61,29 @@ function fileSelected(event) {
 
     var reader = new FileReader();
     reader.onload = function() {
-        var parser = new Parser();
-        try {
-            parser.parse(reader.result);
-        } catch(e) {
-            alert(e);
-            return;
-        }
-
-        startGame(parser.levels);
+        document.getElementById('textInput').value = reader.result;
+        parseAndStartGame(reader.result);
     };
 
     reader.readAsText(file);
 }
 
-function startGame(levels) {
+function runThis() {
+    var str = document.getElementById('textInput').value;
+    parseAndStartGame(str);
+}
+
+function parseAndStartGame(str) {
+    var parser = new Parser();
+    try {
+        parser.parse(str);
+    } catch(e) {
+        alert(e);
+        return;
+    }
+
+    console.log(parser.levels);
+
     // clear file input
     var input = document.getElementById('fileInput');
     try {
@@ -94,15 +94,13 @@ function startGame(levels) {
         }
     } catch(e) {}
 
-    console.log(levels);
-
     document.getElementById('fileInput').blur();
     document.getElementById('runThisButton').blur();
     document.getElementById('textInput').blur();
 
     if(!gamvas.state.getState('game')) {
         // gamvas has not been loaded yet
-        game.loadGame(levels);
+        game.loadGame(parser.levels);
         gamvas.state.addState(game);
         gamvas.state.addState(new pauseState('pause'));
         gamvas.start('gameCanvas', true);
@@ -111,20 +109,7 @@ function startGame(levels) {
     else {
         // gamvas is already running and we're loading a new file
         gamvas.state.setState('game');
-        game.loadGame(levels);
+        game.loadGame(parser.levels);
         game.init();
     }
-}
-
-function runThis() {
-    var str = document.getElementById('textInput').value;
-    var parser = new Parser();
-    try {
-        parser.parse(str);
-    } catch(e) {
-        alert(e);
-        return;
-    }
-
-    startGame(parser.levels);
 }
